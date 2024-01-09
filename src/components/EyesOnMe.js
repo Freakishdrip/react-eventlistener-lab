1a60b28e7850685cd73a0cd6684be5fd5f725717
Nick Drummond <nckdrmmnd@gmail.com>
2024-01-09 16:40:16 -0700

    simple event listener practice

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){ 
+    
+
+    function EyeballsOnME(){
+        console.log("Good!")
+    }
+    function EyeballsAWAYfromME(){
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <div>
+            <button onBlur={EyeballsAWAYfromME} onFocus={EyeballsOnME}>Eyes on me.</button>
+        </div>
+    );
+};
+
+export default EyesOnMe;
